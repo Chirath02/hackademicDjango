f70ee13e9bde80b7115a11e934e47fae36f22dbc
chirath <chirath.02@gmail.com>
2016-04-05 21:15:49 +0530

    Created_by and modified_by to Article table

--- article/models.py
+++ article/models.py
@@ -5,14 +5,14 @@ from django.utils.translation import ugettext_lazy as _
 class Articles(models.Model):
 
     title = models.CharField(_('Title'), max_length=255)
-    content = models.TextField()
+    content = models.TextField(_('Contents'))
 
-    date_added = models.DateTimeField(auto_now_add=True, auto_now=False)
-    created_by = models.ForeignKey('accounts.User')
-    date_modified = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
-    modified_by = models.ForeignKey('accounts.User')
-    ordering = models.IntegerField()
-    is_published = models.BooleanField(default=False)
+    date_added = models.DateTimeField(_('Date Created'), auto_now_add=True, auto_now=False)
+    created_by = models.ForeignKey('accounts.User', related_name='article_created_by', blank=True, null=True)
+    date_modified = models.DateTimeField(_('Date Modified'), auto_now_add=False, auto_now=True, null=True)
+    modified_by = models.ForeignKey('accounts.User', related_name='article_modified_by', blank=True, null=True)
+    ordering = models.IntegerField(_('Ordering'), )
+    is_published = models.BooleanField(_('Published?'), default=False)
 
     def __unicode__(self):
         return self.title
