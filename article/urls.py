bc609ed2b158037cb956baf71c9c04d34c3dd16e
chirath <chirath.02@gmail.com>
2016-04-09 20:03:57 +0530

    Url for articles

---
+++ article/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from article import views
+
+
+urlpatterns = [
+    url(r'^article/$', views.artilce_list),
+    url(r'^article/(?P<pk>[0-9]+)/$', views.article_detail),
+]
