2be626cb04cfe464846a5db64029bd1c0ea7df7c
chirath <chirath.02@gmail.com>
2016-04-09 20:39:26 +0530

    Fixed typo in urls.py

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,6 @@ from article import views
 
 
 urlpatterns = [
-    url(r'^article/$', views.artilce_list),
-    url(r'^article/(?P<pk>[0-9]+)/$', views.article_detail),
+    url(r'article/$', views.artilce_list),
+    url(r'article/(?P<pk>[0-9]+)/$', views.article_detail),
 ]
