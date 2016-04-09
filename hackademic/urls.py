6e63baca9d4ba1102f4d43c51f93db3c703f8aa3
chirath <chirath.02@gmail.com>
2016-04-09 20:07:04 +0530

    Added articles to the main urls.py

--- hackademic/urls.py
+++ hackademic/urls.py
@@ -14,30 +14,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework import routers, serializers, viewsets
-from accounts.models import User
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-    # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('snippets.urls'), name='articles'),
 ]
