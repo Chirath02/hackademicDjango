3000397c3729a261c8a5da2ab7e77f2dbdd5fdd4
chirath <chirath.02@gmail.com>
2016-04-15 21:26:14 +0530

    added permission to update articles

--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from article.models import Article
 from article.serializers import ArticleSerializer
 from rest_framework import generics
+from rest_framework import permissions
 
 
 class ArticleList(generics.ListCreateAPIView):
@@ -9,6 +10,7 @@ class ArticleList(generics.ListCreateAPIView):
     '''
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -18,5 +20,6 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     Retrieve, update or delete an article instance
     '''
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
