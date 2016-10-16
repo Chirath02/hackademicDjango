eded6fe0c61db2e2f4b88b5450ab94f514b87c21
Chirath02 <chirath.02@gmail.com>
2016-10-16 21:00:10 +0530

    Competed Class table

--- account/models.py
+++ account/models.py
@@ -3,3 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Class(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    archive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
