8934db7f8e43b45deb03cdf98d9883207e5c0fb0
Chirath02 <chirath.02@gmail.com>
2016-10-17 21:15:26 +0530

    Table for UserHasChallengeTocken

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from challenge.models import Challenge
 
+
+class UserHasChallengeTocken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    token = models.CharField(max_length=300)
+
+    def __str__(self):
+        return "Tocken for " + self.user + " and " + self.challenge
