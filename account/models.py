9bee256e89aaf4a1152acea0712a0c0ebfa032d7
Chirath02 <chirath.02@gmail.com>
2016-10-17 21:19:27 +0530

    Table for UserSCore

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from challenge.models import Challenge
+from classes.models import Class
 
 
 class UserHasChallengeTocken(models.Model):
@@ -10,3 +11,11 @@ class UserHasChallengeTocken(models.Model):
 
     def __str__(self):
         return "Tocken for " + self.user + " and " + self.challenge
+
+class UserScore(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    class_name = models.ForeignKey(Class)
+
+    def __str__(self):
+        return "Score for " + self.user
