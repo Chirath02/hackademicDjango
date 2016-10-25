81bd958ef3961fc0509ff1a4819e8c214ff05a1e
Chirath02 <chirath.02@gmail.com>
2016-10-25 23:54:11 +0530

    added tables of classes in admin

--- classes/admin.py
+++ classes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Class, ClassChallenge, ClassMembership
 
 # Register your models here.
+
+admin.site.register(Class)
+admin.site.register(ClassMembership)
+admin.site.register(ClassChallenge)
