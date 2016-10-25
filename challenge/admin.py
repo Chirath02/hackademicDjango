eb9386ecd7a4c63fad91bcebcaa618e8949bd6b5
Chirath02 <chirath.02@gmail.com>
2016-10-25 23:55:58 +0530

    added tables of challenges in admin

--- challenge/admin.py
+++ challenge/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Challenge
+from .models import Challenge, ChallengeAttempts, ScoringRules
 
 
 admin.site.register(Challenge)
+admin.site.register(ChallengeAttempts)
+admin.site.register(ScoringRules)
