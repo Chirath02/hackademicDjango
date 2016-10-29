99d25e917e235877b5b49d4c8dc4ffedc2e782e0
Chirath02 <chirath.02@gmail.com>
2016-10-29 11:31:57 +0530

    added serilizers.py to articles

--- article/views.py
+++ article/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import ListAPIView
+from .serializers import ArticleSerializer
 from .models import Article
 
 
 class ArticleListAPIView(ListAPIView):
     queryset = Article.objects.all()
-    
+    serializer_class = ArticleSerializer
+
