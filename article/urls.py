58a4c6d45c550ea36e52183cf49ccf1d49b83066
Chirath02 <chirath.02@gmail.com>
2016-11-01 22:45:32 +0530

    Added detailview for articles

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,7 @@
-from .views import ArticleListAPIView
+from .views import ArticleListAPIView, ArticleDetailAPIView
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', ArticleListAPIView.as_view(), name='articles_list'),
+    url(r'^(?P<pk>\d+)/$', ArticleListAPIView.as_view(), name='article_detail'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,9 +1,14 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .serializers import ArticleSerializer
 from .models import Article
 
 
 class ArticleListAPIView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetailAPIView(RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
