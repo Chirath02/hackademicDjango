e6c27576584d196a6702ff741b46255c9cf9accf
Chirath02 <chirath.02@gmail.com>
2016-11-01 23:01:18 +0530

    url for index view index.html

--- hackademic/settings.py
+++ hackademic/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'hackademic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['client'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
