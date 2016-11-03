42572ccc3347428f29234a6afd3c355de268d2cb
Chirath02 <chirath.02@gmail.com>
2016-11-03 22:20:40 +0530

    added media root and static root

--- hackademic/urls.py
+++ hackademic/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from .views import IndexView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     url(r'^article/', include('article.urls')),
     url(r'^challenge/', include('challenge.urls')),
     url(r'^$', IndexView.as_view(), name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
