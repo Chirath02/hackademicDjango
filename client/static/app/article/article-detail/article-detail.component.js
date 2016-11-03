c357dbdf771facad1416487a058c74968b85c321
Chirath02 <chirath.02@gmail.com>
2016-11-03 23:59:33 +0530

    added new article-detail controller

--- client/static/app/article/article-detail/article-detail.component.js
+++ client/static/app/article/article-detail/article-detail.component.js
@@ -6,8 +6,10 @@
 angular.module('articleDetail').
     component('articleDetail', {
         templateUrl: 'static/app/article/article-detail/article-detail.template.html',
-        controller: function ($scope, $http) {
-            $http.get();
+        controller: function ($scope, $http, $routeParams) {
+            $http.get('/article/' + $routeParams.id + '/' ).then(function (response) {
+                $scope.article = response.data;
+            })
         }
     }
 );
